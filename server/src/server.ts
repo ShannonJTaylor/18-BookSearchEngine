6056180a08b1b44d92e944bb04e2007cfa071a8a
ShannonJTaylor <shannonjo8475@gmail.com>
2025-02-06 15:11:14 -0500

    Removed auo README generator. Not this time.

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
-import { generateReadme } from './readmeGenerator.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
