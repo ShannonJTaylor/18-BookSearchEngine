551148d6b9f8424805ee52feb04f5721f684f2a2
ShannonJTaylor <shannonjo8475@gmail.com>
2025-03-12 20:29:24 -0400

    deatabase issues

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
-
+console.log(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+
 
 export default mongoose.connection;
